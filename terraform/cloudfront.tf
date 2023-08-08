58bda160f17f8ebaad68eadfcfe388ec901b88f0
Eike Kohlmeyer <eike.kohlmeyer@verbraucherritter.de>
2023-08-08 19:38:34 +0200

    Handle 404 for routing in SPA

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -26,11 +26,19 @@ resource "aws_cloudfront_distribution" "kohlmeyer-ai" {
 
   }
 
+  custom_error_response {
+    error_caching_min_ttl = 10
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+  
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
   }
+
   viewer_certificate {
     acm_certificate_arn = "arn:aws:acm:us-east-1:014081376711:certificate/67389b1d-6dfd-44ce-8de6-ee9d1a5521f9"
     ssl_support_method  = "sni-only"
